dfbd193e17704bbb5d72f2aee9a25ab372858c74
artbe <art.beloded@gmail.com>
2024-11-01 12:00:41 +0200

    Add project files.

---
+++ Commentium.API/Program.cs
@@ -0,0 +1,27 @@
+
+namespace Commentium.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
